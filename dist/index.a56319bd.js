62ba1599b64c37716357a8558527ed4bb975f7d2
Radek <radek42310o@gmail.com>
2022-06-12 20:45:20 +0200

    added more functionality and content

--- dist/index.a56319bd.js
+++ dist/index.a56319bd.js
@@ -506,6 +506,9 @@ function hmrAcceptRun(bundle, id) {
 const expandQuizzesListBtn = document.querySelector(".nav__change-quiz-btn");
 const quizzesListSection = document.querySelector(".quizes-list-section");
 expandQuizzesListBtn.addEventListener("click", ()=>{
+    const interactivityStatusOfPage = document.body.dataset.notInteractive;
+    document.body.dataset.notInteractive = true;
+    if (interactivityStatusOfPage) document.body.dataset.notInteractive = false;
     quizzesListSection.classList.toggle("active");
 });
 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,5 +2,10 @@ const expandQuizzesListBtn = document.querySelector('.nav__change-quiz-btn');
 const quizzesListSection = document.querySelector('.quizes-list-section');
 
 expandQuizzesListBtn.addEventListener('click', () => {
+    const interactivityStatusOfPage = document.body.dataset.notInteractive;
+    document.body.dataset.notInteractive = true;
+    if (interactivityStatusOfPage) {
+        document.body.dataset.notInteractive = false;
+    }
     quizzesListSection.classList.toggle('active');
 });
