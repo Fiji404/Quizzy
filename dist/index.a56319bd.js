b2f62003c7f0795c3c3afaa5fc49b46f66f3d2c4
Radek <radek42310o@gmail.com>
2022-06-13 15:44:32 +0200

    landing page finish, mobile fixes

--- dist/index.a56319bd.js
+++ dist/index.a56319bd.js
@@ -504,12 +504,18 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"bAabt":[function(require,module,exports) {
 const expandQuizzesListBtn = document.querySelector(".nav__change-quiz-btn");
-const quizzesListSection = document.querySelector(".quizes-list-section");
+const backFromQuizesBtn = document.querySelector(".nav__back-from-quizes");
+const quizesContainer = document.querySelector(".available-quizzes-grid");
+const bodyElement = document.body;
 expandQuizzesListBtn.addEventListener("click", ()=>{
-    const interactivityStatusOfPage = document.body.dataset.notInteractive;
-    document.body.dataset.notInteractive = true;
-    if (interactivityStatusOfPage) document.body.dataset.notInteractive = false;
-    quizzesListSection.classList.toggle("active");
+    bodyElement.classList.add("not-interactive");
+    quizesContainer.scrollIntoView({
+        behavior: "smooth",
+        top: "-50px"
+    });
+});
+backFromQuizesBtn.addEventListener("click", ()=>{
+    bodyElement.classList.remove("not-interactive");
 });
 
 },{}]},["dOzAP","bAabt"], "bAabt", "parcelRequiree238")
