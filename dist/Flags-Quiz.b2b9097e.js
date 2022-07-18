fa67de6c0d261952930edbb326efc63752eafddd
Fiji404 <87615863+Fiji404@users.noreply.github.com>
2022-07-18 20:21:32 +0200

    Delete dist directory

--- dist/Flags-Quiz.b2b9097e.js
+++
@@ -1,8 +0,0 @@
-const questionsNumberlist = document.querySelectorAll(".settings-options-list");
-questionsNumberlist.forEach((el)=>{
-    el.addEventListener("click", ()=>{
-        el.classList.add("active");
-    });
-});
-
-//# sourceMappingURL=Flags-Quiz.b2b9097e.js.map
